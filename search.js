2ba139118ddf9f6e1baf1d670ee081bd670f804d
PaironCorp <91694823+PaironCorp@users.noreply.github.com>
2025-12-10 13:44:23 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,7 +1,11 @@
 function search(query, data) {
   query = query.trim();
+
+  if (!query) return []; 
+
   return data.filter(item => item.toLowerCase().includes(query.toLowerCase()));
 }
 
 module.exports = search;
+
 // Basic search function
